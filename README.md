14a653315eab01804bddf6502da26ec7d4d2aefc
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2023-10-01 23:12:47 +0300

    Extract routes to separate Routes class. Update readme.

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-## Example Project build on Typeleve
+## Example Project build on Typelevel Stack
 
 ### Usage
 
 This is a normal sbt project. You can compile code with `sbt compile`, run it with `sbt run`, and `sbt console` will start a Scala 3 REPL.
+
+Using the command to run main app:
+```sbt
+sbt "runMain com.example.jobsborad.Application"
+```
--- src/main/scala/example/project/jobsboard/Application.scala
+++ src/main/scala/example/project/jobsboard/Application.scala
@@ -1,30 +1,13 @@
 package example.project.jobsboard
 
 import cats.effect.{ IO, IOApp }
-import io.circe.generic.auto.*
-import io.circe.syntax.*
-import org.http4s.dsl.Http4sDsl
-import org.http4s.circe.*
-import cats.Monad
-import cats.data.Kleisli
-import org.http4s.{ HttpRoutes, Request, Response }
+import example.project.jobsboard.http.routes.HealthRoutes
 import org.http4s.ember.server.EmberServerBuilder
-import org.http4s.server.Router
 
 object Application extends IOApp.Simple:
-  private def healthEndpoint[F[_]: Monad]: HttpRoutes[F] = {
-    val dsl = Http4sDsl[F]
-    import dsl.*
-    HttpRoutes.of[F] { case GET -> Root / "health" =>
-      Ok("All going great!")
-    }
-  }
-
-  private def routerWithPathPrefix = Router("/private" -> healthEndpoint[IO]).orNotFound
-
   def run: IO[Unit] =
     EmberServerBuilder
       .default[IO]
-      .withHttpApp(routerWithPathPrefix)
+      .withHttpApp(HealthRoutes[IO].routes.orNotFound)
       .build
       .use(s => IO.println(s"Server started at: ${s.address}") *> IO.never)
