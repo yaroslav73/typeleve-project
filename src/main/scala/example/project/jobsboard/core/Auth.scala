81bf17ceb9e6eebb6573d2dedd7060da2dbcbb81
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2024-04-02 21:56:12 +0300

    Addded auth basic implementation and testse.

--- src/main/scala/example/project/jobsboard/core/Auth.scala
+++ src/main/scala/example/project/jobsboard/core/Auth.scala
@@ -1,5 +1,6 @@
 package example.project.jobsboard.core
 
+import cats.syntax.all.*
 import tsec.authentication.AugmentedJWT
 import tsec.mac.jca.HMACSHA256
 import example.project.jobsboard.domain.Auth.NewPasswordInfo
@@ -7,6 +8,10 @@ import example.project.jobsboard.domain.User
 import example.project.jobsboard.domain.User.New
 import example.project.jobsboard.domain.Aliases.JwtToken
 import example.project.jobsboard.domain.Aliases.Authenticator
+import cats.FlatMap
+import cats.Applicative
+import cats.Monad
+import cats.Functor
 
 trait Auth[F[_]]:
   def login(email: String, password: String): F[Option[JwtToken]]
@@ -14,10 +19,32 @@ trait Auth[F[_]]:
   def changePassword(email: String, newPassword: NewPasswordInfo): F[Either[String, Option[User]]]
 
 object Auth:
-  def make[F[_]](users: Users[F], authenticator: Authenticator[F]): Auth[F] = new Auth[F] {
-    def login(email: String, password: String): F[Option[JwtToken]] = ???
+  def make[F[_]: Monad](users: Users[F], authenticator: Authenticator[F]): Auth[F] = new Auth[F] {
+    def login(email: String, password: String): F[Option[JwtToken]] =
+      users.find(email).flatMap {
+        case Some(user) if user.hashedPassword == password =>
+          authenticator.create(user.email).map(_.some)
+        case _ =>
+          Applicative[F].pure(None)
+      }
 
-    def signUp(user: New): F[Option[User]] = ???
+    def signUp(user: New): F[Option[User]] =
+      users.find(user.email).flatMap {
+        case Some(_) => Applicative[F].pure(None)
+        case _ =>
+          for {
+            userId <- users.create(user)
+            user   <- users.find(userId)
+          } yield user
+      }
 
-    def changePassword(email: String, newPassword: NewPasswordInfo): F[Either[String, Option[User]]] = ???
+    def changePassword(email: String, passwordInfo: NewPasswordInfo): F[Either[String, Option[User]]] =
+      users.find(email).flatMap {
+        case Some(user) if user.hashedPassword == passwordInfo.oldPassword =>
+          users.update(user.copy(hashedPassword = passwordInfo.newPassword)).map(_.asRight)
+        case Some(_) =>
+          Applicative[F].pure("Invalid password".asLeft)
+        case _ =>
+          Applicative[F].pure("User with this email not found".asLeft)
+      }
   }
---
+++ src/test/scala/example/project/jobsboard/core/AuthSpec.scala
@@ -0,0 +1,125 @@
+package example.project.jobsboard.core
+
+import cats.implicits.*
+import org.scalatest.matchers.should.Matchers
+import example.project.jobsboard.fixtures.UserFixture
+import cats.effect.testing.scalatest.AsyncIOSpec
+import org.scalatest.freespec.AsyncFreeSpec
+import cats.effect.IO
+import java.util.UUID
+import example.project.jobsboard.domain.User
+import example.project.jobsboard.domain.Aliases.Authenticator
+import tsec.mac.jca.HMACSHA256
+import tsec.authentication.IdentityStore
+import cats.data.OptionT
+import tsec.authentication.JWTAuthenticator
+import concurrent.duration.DurationInt
+import example.project.jobsboard.domain.Auth.NewPasswordInfo
+
+class AuthSpec extends AsyncFreeSpec with AsyncIOSpec with Matchers with UserFixture {
+  "Auth 'algebra'" - {
+    "login should return None if the user does not exists" in {
+      val auth  = Auth.make[IO](testUsers, authenticator)
+      val token = auth.login(NotFoundUserEmail, john.hashedPassword)
+
+      token.asserting(_ shouldBe None)
+    }
+
+    "login should return None if the user password is wrong" in {
+      val auth  = Auth.make[IO](testUsers, authenticator)
+      val token = auth.login(john.email, "wrongpassword")
+
+      token.asserting(_ shouldBe None)
+    }
+
+    "login should return token if the user exists and password is correct" in {
+      val auth  = Auth.make[IO](testUsers, authenticator)
+      val token = auth.login(john.email, john.hashedPassword)
+
+      token.asserting(_ shouldBe defined)
+    }
+
+    "signup should not create user if the email already exists" in {
+      val auth = Auth.make[IO](testUsers, authenticator)
+      val userId = auth.signUp(
+        User.New.recruiter(
+          email          = john.email,
+          hashedPassword = "123456789",
+          firstName      = Some("Test"),
+          lastName       = Some("Name"),
+          company        = Some("Test Company"),
+        )
+      )
+
+      userId.asserting(_ shouldBe None)
+    }
+
+    "signup should create user" in {
+      val auth = Auth.make[IO](testUsers, authenticator)
+      val userId = auth.signUp(
+        User.New.recruiter(
+          email          = NotFoundUserEmail,
+          hashedPassword = "123456789",
+          firstName      = Some("Test"),
+          lastName       = Some("Name"),
+          company        = Some("Test Company"),
+        )
+      )
+
+      userId.asserting(_ shouldBe defined)
+    }
+
+    "changePassword should return error if user does not exists" in {
+      val auth   = Auth.make[IO](testUsers, authenticator)
+      val result = auth.changePassword(NotFoundUserEmail, NewPasswordInfo("oldpassword", "newpassword"))
+
+      result.asserting(_ shouldBe Left("User with this email not found"))
+    }
+
+    "changePassword should return error if user old password does not match" in {
+      val auth   = Auth.make[IO](testUsers, authenticator)
+      val result = auth.changePassword(john.email, NewPasswordInfo("oldpassword", "newpassword"))
+
+      result.asserting(_ shouldBe Left("Invalid password"))
+    }
+
+    "changePassword should change password if user exists" in {
+      val auth   = Auth.make[IO](testUsers, authenticator)
+      val result = auth.changePassword(john.email, NewPasswordInfo(john.hashedPassword, "newpassword"))
+
+      result.asserting(_ shouldBe Right(Some(john)))
+    }
+  }
+
+  private val testUsers: Users[IO] = new Users[IO] {
+    def find(userId: UUID): IO[Option[User]] =
+      if (userId == john.id) IO.pure(Some(john)) else IO.pure(None)
+    def find(email: String): IO[Option[User]] =
+      if (email == john.email) IO.pure(Some(john)) else IO.pure(None)
+    def create(user: User.New): IO[UUID] =
+      IO.pure(john.id)
+    def update(user: User): IO[Option[User]] =
+      if (user.id == john.id) IO.pure(Some(john)) else IO.pure(None)
+    def delete(id: UUID): IO[Boolean] =
+      if (id == john.id) IO.pure(true) else IO.pure(false)
+  }
+
+  private val authenticator: Authenticator[IO] = {
+    // 1. Key for hashing
+    val key = HMACSHA256.unsafeGenerateKey
+    // 2. Indentity store to retrieve users
+    val idStore: IdentityStore[IO, String, User] = (email: String) =>
+      email match {
+        case john.email => OptionT.pure(john)
+        case anna.email => OptionT.pure(anna)
+        case _          => OptionT.none[IO, User]
+      }
+    // 3. jwt authenticator
+    JWTAuthenticator.unbacked.inBearerToken(
+      1.day,
+      None,
+      idStore,
+      key,
+    )
+  }
+}
